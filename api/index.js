cf63afee6fb8c88c43e5e64d6369df738ac9c435
root <root@DESKTOP-SSD.localdomain>
2020-09-12 20:05:02 +0300

    test

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
+  const { r = '' } = req.query
+  for(i in r)for(j in r)l=[...a],l[i]=a[j],l[j]=a[i],l=l.join``,r=r>l?r:l;
+  res.status(200).send(+r+1&&r||'-')
 }
 
