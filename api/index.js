627155fb10d7c8a688a28f298266acc35779304a
root <root@DESKTOP-SSD.localdomain>
2020-09-12 20:54:27 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
-  const { r = '' } = req.query
+  const { a = '' } = req.query;
+  let r = a;
+  let l = [];
   for(i in r)for(j in r)l=[...a],l[i]=a[j],l[j]=a[i],l=l.join``,r=r>l?r:l;
-  res.status(200).send(+r+1&&r||'-')
+  res.status(200).send(+r+1&&r||'-');
 }
 
